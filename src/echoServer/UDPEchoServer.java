f458781045d4d05df4fecb3817512f8704f495fa
Sofia Marinina <sofia.marinina@student.karazin.ua>
2026-05-25 22:54:30 +0300

    First Commit

---
+++ src/echoServer/UDPEchoServer.java
@@ -0,0 +1,34 @@
+package echoServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPEchoServer extends UDPServer {
+    public final static int DEFAULT_PORT = 7;
+
+    public UDPEchoServer() {
+        super(DEFAULT_PORT);
+    }
+
+    @Override
+    public void respond(DatagramSocket socket, DatagramPacket request) throws IOException {
+        DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+        socket.send(reply);
+    }
+
+    public static void main(String[] args) {
+        UDPServer server = new UDPEchoServer();
+        Thread t = new Thread(server);
+        t.start();
+        System.out.println("Початок ехо-сервера...");
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        server.shutDown();
+        System.out.println("Кінець ехо-сервера...");
+    }
+}
---
+++ src/echoServer/UDPServer.java
@@ -0,0 +1,50 @@
+package echoServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+public abstract class UDPServer implements Runnable {
+    private final int bufferSize;
+    private final int port;
+    private volatile boolean isShutDown = false;
+
+    public UDPServer(int port, int bufferSize) {
+        this.port = port;
+        this.bufferSize = bufferSize;
+    }
+
+    public UDPServer(int port) {
+        this(port, 8192);
+    }
+
+    public void shutDown() {
+        this.isShutDown = true;
+    }
+
+    public abstract void respond(DatagramSocket socket, DatagramPacket request) throws IOException;
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[bufferSize];
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            socket.setSoTimeout(10000);
+            while (true) {
+                if (isShutDown) return;
+                DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+                try {
+                    socket.receive(incoming);
+                    this.respond(socket, incoming);
+                } catch (SocketTimeoutException ex) {
+                    if (isShutDown) return;
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage() + "\n" + ex);
+                }
+            }
+        } catch (SocketException ex) {
+            System.err.println("Не вдалося прив'язатися до порту: " + port + "\n" + ex);
+        }
+    }
+}
